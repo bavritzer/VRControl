f0008e6154bc63f737e78c4a34a68ad432dbdbb8
ba <ba334@cornell.edu>
2023-12-30 23:54:47 -0500

    added window closing using tkinter

--- potatoserver.py
+++ potatoserver.py
@@ -1,14 +1,15 @@
 from http.server import CGIHTTPRequestHandler, HTTPServer
 import os
-import numpy as np
 from PIL import Image
 from diffusers import StableDiffusionControlNetPipeline, ControlNetModel, UniPCMultistepScheduler
 import torch
 from diffusers.utils import load_image
 from diffusers.pipelines.stable_diffusion import StableDiffusionSafetyChecker
 import socket
-import ctypes
 import datetime
+import tkinter as tk
+from tkinter import messagebox
+import threading
 Image.LOAD_TRUNCATED_IMAGES = True
 
 inputcount = 0
@@ -22,6 +23,13 @@ inputpath = ""
 outputpath = ""
 modinputpath = ""
 date = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S").replace(' ', '').replace('-', '').replace(':', '')
+
+class createBox(threading.Thread):
+    def run(self):
+        root = tk.Tk()
+        resp = messagebox.showinfo(title = 'End Session', message= "Server is now running. Press OK to end session.", type = messagebox.OK, parent = root)
+        if resp == 'ok':
+            os._exit(0)
 
 class PotatoHTTPServer(CGIHTTPRequestHandler):
 
@@ -72,22 +80,29 @@ class PotatoHTTPServer(CGIHTTPRequestHandler):
             self.wfile.write(f.read())
 
 if __name__ == "__main__":
-    ctypes.windll.user32.MessageBoxW(0, "By clicking OK, you agree to the terms and conditions of the model\n license laid out in the ReadMe included in this repository.", "Initializing")
+    resp = messagebox.showinfo(message = "By clicking OK, you agree to the terms and conditions of the AI model licenses laid out in the ReadMe included in this repository at https://github.com/bavritzer/VRControl.", title = "Initializing", type = messagebox.OKCANCEL)
+    if resp=='cancel':
+        exit(0)
     if not os.path.exists("inputs"):
         os.mkdir("inputs")
     if not os.path.exists("modinputs"):
         os.mkdir("modinputs")
     if not os.path.exists("outputs"):
         os.mkdir("outputs")
+    resp = messagebox.showinfo(message = "Would you like to disable the safety checker? This will speed up image generation and eliminate any blank images but may produce content unsuitable for certain audiences such as minors. By disabling this feature, you acknowledge that you have read the terms and conditions of usage as described at https://github.com/bavritzer/VRControl and certify that your usage complies with those terms.", title = "Disable Safety Checker?", type = messagebox.YESNO, default=messagebox.NO)
     inputpath = os.path.abspath("inputs")
     modinputpath = os.path.abspath("modinputs")
     outputpath = os.path.abspath("outputs")
     controlnet = ControlNetModel.from_pretrained("lllyasviel/sd-controlnet-scribble", torch_dtype=torch.float16)
-    pipe = StableDiffusionControlNetPipeline.from_pretrained("runwayml/stable-diffusion-v1-5", controlnet=controlnet, safety_checker=StableDiffusionSafetyChecker.from_pretrained("CompVis/stable-diffusion-safety-checker"), torch_dtype=torch.float16)
-    #pipe = StableDiffusionControlNetPipeline.from_pretrained("runwayml/stable-diffusion-v1-5", controlnet=controlnet, safety_checker=None, torch_dtype=torch.float16)
+    if resp == 'yes':
+        pipe = StableDiffusionControlNetPipeline.from_pretrained("runwayml/stable-diffusion-v1-5", controlnet=controlnet, safety_checker=None, torch_dtype=torch.float16)
+    else: 
+        pipe = StableDiffusionControlNetPipeline.from_pretrained("runwayml/stable-diffusion-v1-5", controlnet=controlnet, safety_checker=StableDiffusionSafetyChecker.from_pretrained("CompVis/stable-diffusion-safety-checker"), torch_dtype=torch.float16)
     pipe.scheduler = UniPCMultistepScheduler.from_config(pipe.scheduler.config)
-    pipe.enable_model_cpu_offload()
-    pipe.to(device)
+    if(device=='cpu'):
+        pipe.enable_model_cpu_offload()
+    else:
+        pipe.to(device)
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     s.connect(("8.8.8.8", 80))
     idn = s.getsockname()[0]
@@ -95,8 +110,14 @@ if __name__ == "__main__":
     idm = idn[-3:]
     idm = idm.replace('.', '')
     idm = str(int(idm)*keymag)
-    ctypes.windll.user32.MessageBoxW(0, idm, "Please Enter This Code When Prompted on VRPaint")
+    messagebox.showinfo(title="Access Code", message = "Please Enter This Code When Prompted on VRControl: \n\n"+idm)
     server_address = (idn, 8080)
     httpd = HTTPServer(server_address, PotatoHTTPServer)
     print('Serving at '+str(idn)+':8080')
-    httpd.serve_forever()
+    createBox().start()
+    threading.Thread(target = httpd.serve_forever(), daemon = True).start()
+    
+
+   
+    
+    
